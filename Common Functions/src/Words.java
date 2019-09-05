00ca321f111d5849c794d4be9ae324eb7c13165f
connor <connorfis02@gmail.com>
2019-09-05 19:41:18 -0400

    Common

---
+++ Common Functions/src/Words.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+public class Words {
+    public static void main(String[] args) { //Main function good to add the scanner just in case
+        Scanner sin = new Scanner(System.in);
+    }
+
+    "\t" //Space when outputting one line
+
+    public static void main(String[] args) {                             //File I/O main
+        Scanner sin=new Scanner(new FileReader("81aIN.txt"));;
+    }
+
+
+        System.out.println(stuff to display);
+        if (condition){
+
+        }
+
+        for (newthing; condition; increment){ // for new thing put either int,float,double,boolen, char
+
+        }
+
+        public static int{ // returns whole money
+
+        }
+        public static float{ // returns decimal
+
+}
+        public static double{ // returns better decimal
+
+        }
+
+        public static String {
+
+        }
+
+        public static Boolean{ // return true or false
+
+        }
+        public static void{ //return nothing
+
+        }
+        public static char{ //return character
+
+        }
+    }
+
+
